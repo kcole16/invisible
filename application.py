749457e6117f0174446fae6478c589e7722df8a2
Kendall Cole <kcole16@gmail.com>
2016-03-18 14:53:41 +0700

    Setup

--- application.py
+++ application.py
@@ -4,38 +4,34 @@ import urllib
 import requests
 
 from flask import Flask, jsonify, render_template, redirect, request
-from utils import connect_db, getenv, get_account, update_account
-from message import initial_response
+from utils import connect_db, getenv
+from message import initial_response, update_conversation, create_response
 
 application = Flask(__name__) #establishing flask application
-
-convo_ids = []
-conversations = {}
 
 @application.route('/home/', methods=['GET'])
 def home():
     # if request.method == 'POST':
     return jsonify(data="Hello World")
 
-def respond_to_message(message, convo_id):
-    response = message+" +1"
+def respond_to_message(message, convo_id, response_num):
+    response = create_response(message, response_num)
     access_token = getenv('ACCESS_TOKEN')
     data = {'access_token': access_token, 'message': response}
     url = 'https://graph.facebook.com/v2.5/%s/messages' % convo_id
     r = requests.post(url, data=data) 
+    print r.text
 
-def get_message(message_id, convo_id):
-    print message_id
+def get_message(message_id, convo_id, response_num):
     access_token = getenv('ACCESS_TOKEN')
     params = {'access_token': access_token, 'fields':'message'}
     url = 'https://graph.facebook.com/v2.5/%s' % message_id
     r = requests.get(url, params=params)
     message = r.json()['message']
-    print message
-    respond_to_message(message, convo_id)
+    respond_to_message(message, convo_id, response_num)
+    update_conversation(convo_id, response_num)
 
-def get_messages(convo_id):
-    print convo_id
+def get_messages(convo_id, response_num):
     access_token = getenv('ACCESS_TOKEN')
     params = {'access_token': access_token, 'fields':'from'}
     url = 'https://graph.facebook.com/v2.5/%s/messages' % convo_id
@@ -44,11 +40,10 @@ def get_messages(convo_id):
     newest_message = data[0]
     id = newest_message['id']
     if newest_message['from']['name'] != 'Invisible':
-        get_message(id, convo_id)
+        get_message(id, convo_id, response_num)
 
 def get_conversations():
-    global conversations
-    global convo_ids
+    db = connect_db()
     access_token = getenv('ACCESS_TOKEN')
     params = {'access_token': access_token}
     page_id = '102940933437533'
@@ -56,19 +51,13 @@ def get_conversations():
     r = requests.get(url, params=params)
     data = r.json()['data']
     for convo in data:
-        id = convo['id']
-        if id not in convo_ids:
-            conversation = {
-                'id':convo['id'],
-                'updated_time':convo['updated_time']
-            }
-            conversations[id] = {'updated_time': convo['updated_time']}
-            convo_ids.append(id)
+        id = str(convo['id'])
+        conversation = db.conversations.find_one({'convo_id':id})
+        if conversation is None:
+            db.conversations.insert({'convo_id':id, 'updated_time': convo['updated_time'], 'response':0})
             initial_response(id)
-        else:
-            if convo['updated_time'] != conversations[id]['updated_time']:
-                get_messages(id)
-                conversations[id]['updated_time'] = convo['updated_time']
+        elif convo['updated_time'] != conversation['updated_time']:
+            get_messages(id, conversation['response'])
 
 if __name__ == "__main__":
     application.debug = True
--- message.py
+++ message.py
@@ -1,6 +1,16 @@
 import requests
 
-from utils import getenv
+from utils import getenv, connect_db
+
+def update_conversation(convo_id, response_num):
+    db = connect_db()
+    access_token = getenv('ACCESS_TOKEN')
+    params = {'access_token': access_token}
+    url = 'https://graph.facebook.com/v2.5/%s' % convo_id
+    r = requests.get(url, params=params) 
+    data = r.json()
+    db.conversations.update({'convo_id':convo_id}, {'convo_id':convo_id, 
+        'updated_time':data['updated_time'], 'response': response_num+1})
 
 def initial_response(convo_id):
     response = """Hello! Thanks for trying out the Invisible API demo! This is a basic version of our full product, but hopefully it give you a good idea of what we do! First off, what is your name?"""
@@ -8,3 +18,43 @@ def initial_response(convo_id):
     data = {'access_token': access_token, 'message': response}
     url = 'https://graph.facebook.com/v2.5/%s/messages' % convo_id
     r = requests.post(url, data=data) 
+    if r.ok:
+        update_conversation(convo_id, 0)
+
+def weather_response(location):
+    api_key = getenv('WEATHER_API_KEY')
+    params = {'APPID': api_key, 'q':location, 'units':'metric'}
+    url = 'http://api.openweathermap.org/data/2.5/weather'
+    r = requests.get(url, params=params)
+    data = r.json()
+    weather = data['weather'][0]['description']
+    high = data['main']['temp_max']
+    low = data['main']['temp_min']
+    response = "Today in %s: %s. The high is %s and the low is %s. Have a great day!" % (
+            location,
+            weather,
+            high, 
+            low
+        )
+    return response
+
+
+def create_response(info, response_num):
+    if response_num == 1:
+        response = "Hi %s! To give you an idea of what I can do, I'm going to show you today's weather forecast. Where are you located?" % info
+    elif response_num == 2:
+        response = weather_response(info)
+    elif response_num == 3:
+        response = "That's all for now! This is just a very small sneak peak of our potential. Check out our website for more details: http://www.invisibleapi.com. If you have any questions, send them to kendall@invisibleapi.com."
+    elif response_num == 4:
+        response = "Thanks again for your interest! I'm going to start repeating myself now..."
+    elif response_num == 5:
+        response = "Repeating"
+    elif response_num == 6:
+        response = "Still repeating"
+    else:
+        base = "still "
+        root = base * (response_num - 6)
+        response = "Still "+root+"repeating"
+
+    return response
